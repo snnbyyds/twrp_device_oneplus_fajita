d92ad31fdf799df81f60395ef854269e5a04aaa7
siddhrsh <skoyande17@gmail.com>
2021-09-09 23:19:42 +0530

    OOS11 Test Build

--- omni_fajita.mk
+++ omni_fajita.mk
@@ -17,10 +17,12 @@
 # Release name
 PRODUCT_RELEASE_NAME := fajita
 
-$(call inherit-product, build/target/product/aosp_base.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oneplus/fajita/device.mk)
